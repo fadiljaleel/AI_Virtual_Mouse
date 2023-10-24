cd3d6a47cc4a6d34258c042b287e5f96fae6ef00
fadiljaleel@gmail.com <Ajaleel@1>
2023-10-24 21:38:12 +0530

    initial commit

---
+++ Requirements.py
@@ -0,0 +1,11 @@
+opencv-python     - pip install opencv-python
+mediapipe         - pip install mediapipe
+pyautogui         - pip install pyautogui
+pip install protobuf==3.20.0
+
+
+5 steps - 35 lines
+
+step1 - Video Camera - 6 lines
+
+
